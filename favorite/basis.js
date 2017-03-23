e55e2379efeb8cb924e00d993c96a0b6a8304bbb
Leesson <shclixian@163.com>
2017-03-23 22:30:21 +0800

    add environment detection: AMD, CommonJS or global

--- favorite/basis.js
+++ favorite/basis.js
@@ -1,5 +1,5 @@
 /**
- * basic javascript
+ * are we in browser?
  */
 var isBrowser =
     // the most fundamental decision: are we in the browser?
@@ -7,3 +7,29 @@ var isBrowser =
     typeof location != "undefined" &&
     typeof document != "undefined" &&
     window.location == location && window.document == document;
+
+/**
+ * define our module base on current environment
+ */
+(function (global, factory) {
+
+    'use strict';
+
+    /* Use AMD */
+    if (typeof define === 'function' && define.amd) {
+        define(function () {
+            return new (factory(global, global.document))();
+        });
+    }
+    /* Use CommonJS */
+    else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = new (factory(global, global.document))();
+    }
+    /* Use Browser */
+    else {
+        global.ModuleName = new (factory(global, global.document))();
+    }
+
+})(typeof window !== 'undefined' ? window : this, function (w, d) {
+    /* module code */
+});
