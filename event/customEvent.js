7a2859f78517e7594c3588424e631c4f7ee11f1a
Leesson <shclixian@163.com>
2017-05-10 15:36:56 +0800

    add empty customEvent

---
+++ event/customEvent.js
@@ -0,0 +1,4 @@
+/**
+ * add custom event to custom object
+ * reference to jQuery or Dojo
+ */
--- event/eventUtil.js
+++ event/eventUtil.js
@@ -5,11 +5,11 @@
  * @type {{addHandler: EventUtil.addHandler, removeHandler: EventUtil.removeHandler}}
  */
 var EventUtil = {
-    addHandler: function(element, type, handler){
-        if (element.addEventListener){
+    addHandler: function (element, type, handler) {
+        if (element.addEventListener) {
             // DOM2
             element.addEventListener(type, handler, false);
-        } else if (element.attachEvent){
+        } else if (element.attachEvent) {
             // IE
             // variable this point to window in handler
             element.attachEvent("on" + type, handler);
@@ -18,10 +18,10 @@ var EventUtil = {
             element["on" + type] = handler;
         }
     },
-    removeHandler: function(element, type, handler){
-        if (element.removeEventListener){
+    removeHandler: function (element, type, handler) {
+        if (element.removeEventListener) {
             element.removeEventListener(type, handler, false);
-        } else if (element.detachEvent){
+        } else if (element.detachEvent) {
             element.detachEvent("on" + type, handler);
         } else {
             element["on" + type] = null;
